fc1f4eef426e879a9e3caa4ca5a907596ee055eb
Gustavo Lima <121981603+GustavoH-Lima@users.noreply.github.com>
2025-09-12 14:55:35 -0300

    Update metricas.sh

--- metricas.sh
+++ metricas.sh
@@ -1,73 +1,75 @@
 #!/bin/bash
 
-#Este programa rodará a versão $1 com os tamanho 1024 até 2^(9 + $2) e armazenará a métrica cujas informações deseja-se coletar
-# $1:Versão  $2:Quantas rodadas $3: Métrica a ser coletada $4 programa a ser executado {./testes_variaveis/pi ou ./mult}
-#$3 pode ser:
+#Este programa rodará a versão $1 com os tamanho 1024 até 2^(09 + $2) e armazenará a métrica cujas informações deseja-se coletar
+# $1:Versão  $2:Quantas rodadas $3 nome da pasta
 
-# scaph_process_power_consumption_microwatts
-# scaph_process_cpu_usage_percentage
-# scaph_process_disk_total_read_bytes
-# scaph_process_disk_total_write_bytes
-# scaph_process_disk_read_bytes
-# scaph_process_disk_write_bytes
-# scaph_process_memory_bytes
-# scaph_process_memory_virtual_bytes
+lista=("scaph_process_power_consumption_microwatts" \
+       "scaph_process_cpu_usage_percentage" \
+       "scaph_process_disk_total_read_bytes" \
+       "scaph_process_disk_total_write_bytes" \
+       "scaph_process_disk_read_bytes" \
+       "scaph_process_disk_write_bytes" \
+       "scaph_process_memory_bytes" \
+       "scaph_process_memory_virtual_bytes")
+inicia_pastas(){
+    mkdir -p "$1"
+    for item in "${lista[@]}"; do
+        mkdir -p "$1/$item/matriz"
+        mkdir -p "$1/$item/pi"
+    done
+}
+
 mede_energia() {
-    # $1: versão   $2: métrica   $3: programa
-    >testes_variaveis/ER_$tamanho
+    # $1: versão  $2: nome do subdir (matriz/pi)  $3: programa $4 pasta base
+    for item in "${lista[@]}"; do
+        >"$4/$item/$2/$tamanho"
+    done
     sudo scaphandre stdout -t-1 -s 1 --raw-metrics > Saida.txt &
     pid=$!
 
     $3 m1 m2 $1 > mult_s &
     pidp=$!
 
-    # Enquanto o programa $3 não terminou
     while kill -0 $pidp 2>/dev/null; do
-        sleep 20   # espera 20 segundos (20 medições)
+        sleep 20
         if [ -s Saida.txt ]; then
-            extrai_numeros $2
-            > Saida.txt   # limpa Saida.txt
+            extrai_numeros $2 $pidp $4
+            > Saida.txt
         fi
     done
 
     wait $pidp
-    kill $pid
+    kill $pid 2>/dev/null
 
-    # processa qualquer sobra no final
     if [ -s Saida.txt ]; then
-        extrai_numeros $2
+        extrai_numeros $2 $pidp $4
         > Saida.txt
     fi
-    rm Saida.txt
-    rm Resultado
+
+    rm -f Saida.txt mult_s
+    rm -f Resultado
 }
 
 
-extrai_numeros()
-{
-    #$1: Métrica coletada $2 nome do programa executado
-    tempo=$(cat mult_s)
-    rm mult_s
-    grep --text -E "$1.*$pidp.*m1m2" Saida.txt >> testes_variaveis/ER_$tamanho
-    #Agora, outra expressão regular para pegar só os números
+extrai_numeros() {
+    # $1: subdir (matriz/pi)  $2: pid $3: base
+    tempo=$(cat mult_s 2>/dev/null)
 
-    # grep -o "[0-9]*\.[0-9]*" ER >medidas
-    # rm ER
-    #Por fim, pego somente os números, divido por 1e6 para transformar em watts, e como as medidas foram feitas de 1 em 1 segundo, eles estão em Joules.
-    #Portanto, ao final do próximo comando, obtenho a quantidade de energia, em Joules, gasta pelo programa.
-    # energia=$(grep -Eo "[0-9]+\.[0-9]+" medidas| awk '{s+=$1/1e6} END {print s}')
-    # rm medidas
+    for item in "${lista[@]}"; do
+        grep --text -E "$item.*$2.*m1m2" Saida.txt >> "$3/$item/$1/$tamanho"
+    done
 }
 
 tamanho=1024
+inicia_pastas $3
 
-for ((i = 1; i<=$2; i++)); do
-    #Colocando a versão a ser rodada
+for ((i = 1; i <= $2; i++)); do
     ./gera m1 $tamanho
     ./gera m2 $tamanho
-    echo "Executando tamanho $tamanho métrica $3"
-    mede_energia $1 $3 $4
-    tamanho=$(( $tamanho * 2 ))
-    clear
+    echo "Executando versão $1 tamanho $tamanho"
+    mede_energia $1 matriz ./mult $3
+    tamanho=$(( tamanho * 2 ))
 done
-echo "Terminado métrica $3" 
+
+mede_energia $1 pi ./testes_variaveis/pi $3
+echo "Terminado"
